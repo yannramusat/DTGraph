c2c836fe1d5f910784a8813ccfbb3dabe075f075
yannramusat <yann.ramusat@ens.fr>
2024-01-18 10:25:59 +0100

    Move credentials to environment

--- examples/main.py
+++ examples/main.py
@@ -1,17 +1,26 @@
 #!/bin/env python3
+import os
 from dtgraph import Neo4jGraph, Rule
 
 if __name__ == "__main__":
-    # local development
+    # Defaults for local development
     scheme = "bolt"
     hostname = "localhost"
     port = "7687"
-    username = None
+    username = "neo4j"
     password = None
-    ## Neo4j sandbox
-    #hostname = "34.200.233.111"
-    #username = "neo4j"
-    #password = "breakdown-certificates-navigations"
+    # Using env. Useful for overriding previous
+    # values when using a Neo4j sandbox
+    if s := os.getenv('DTG_SCHEME'):
+        scheme = s
+    if h := os.getenv('DTG_HOSTNAME'):
+        hostname = h
+    if po := os.getenv('DTG_PORT'):
+        port = po
+    if u := os.getenv('DTG_USERNAME'):
+        username = u
+    if pa := os.getenv('DTG_PASSWORD'):
+        password = pa
 
     uri = f"{scheme}://{hostname}:{port}"
     graph = Neo4jGraph(uri, "neo4j", username=username, password=password, verbose=True)
