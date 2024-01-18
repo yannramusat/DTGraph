cd02acc2f66258803b2fee6afdeca655774f2436
yannramusat <yann.ramusat@ens.fr>
2024-01-18 17:24:50 +0100

    Parsing of id tuples

--- dtgraph/parser.py
+++ dtgraph/parser.py
@@ -8,19 +8,40 @@ import pyparsing as pp
 constant = pp.Combine(pp.Literal('"') + pp.Word(pp.alphanums) + pp.Literal('"'))
 freevar = pp.Word(pp.alphas.lower(), pp.alphanums)
 attribute = pp.Word(pp.alphas, pp.alphanums)
-accesskey = pp.Combine(freevar.set_results_name("freevar") + pp.Literal('.')  + attribute.set_results_name("key"))
+accesskey = pp.Combine(
+    freevar('freevar') + 
+    pp.Literal('.') + 
+    attribute('key'))
 label = pp.Word(pp.alphas.upper(), pp.alphanums)
 
-IDElement = constant | freevar | accesskey | label
+IDElement = constant('value') | accesskey('value') | freevar('value') | label('value')
 
-print(constant.parse_string(' "test" '))
-print(freevar.parse_string(' x '))
-print(accesskey.parse_string(' x1.de1 '))
-print(accesskey.parse_string(' x2.de2 ')['freevar'])
-print(accesskey.parse_string(' x3.de3 ')['key'])
+IDTuple = pp.Suppress('(') + pp.ZeroOrMore(IDElement + pp.Optional(pp.Suppress(',')))('ids') + pp.Suppress(')')
 
-print(IDElement.parse_string(' "test" '))
-print(IDElement.parse_string(' x '))
-print(IDElement.parse_string(' x1.de1 '))
-#print(IDElement.parse_string(' x2.de2 ')['freevar'])
-#print(IDElement.parse_string(' x3.de3 ')['key'])
+try:
+    print("Test base elements:")
+    print(constant.parse_string(' "test" '))
+    print(freevar.parse_string(' x '))
+    print(accesskey.parse_string(' x1.de1 '))
+    print(accesskey.parse_string(' x2.de2 ')['freevar'])
+    print(accesskey.parse_string(' x3.de3 ')['key'])
+    print("Test IDElement:")
+    print(IDElement.parse_string(' "test" ')['value'])
+    print(IDElement.parse_string(' x ')['value'])
+    print(IDElement.parse_string(' x1.de1 ')['value'])
+    print(IDElement.parse_string(' x2.de2 ')['value']['freevar'])
+    print(IDElement.parse_string(' x3.de3 ')['value']['key'])
+    print("Test accesskeys:")
+    res = IDTuple.parse_string('( x1.de1, x2.de2, x3.de3 )')
+    print(res['ids'][1])
+    print(res['ids'][1]['freevar'])
+    print(res['ids'][1]['key'])
+    print("Test IDTuple:")
+    res = IDTuple.parse_string('("test", x, x1.de1, x2.de2, x3.de3, )')
+    print(res['ids'][0])
+    print(res['ids'][1])
+    print(res['ids'][2])
+    print(res['ids'][2]['freevar'])
+    print(res['ids'][2]['key'])
+except pp.ParseException as pe:
+    print("Did not Match: ", pe)
