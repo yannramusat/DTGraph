8e252bcd4a3b646c90a855cd8ffcc52c4a2de82a
yannramusat <yann.ramusat@ens.fr>
2024-01-19 13:47:17 +0100

    Add support for Edge constructors in the DSL

--- dtgraph/parser.py
+++ dtgraph/parser.py
@@ -4,6 +4,7 @@ from pyparsing import *
 #   - We require variables to begin with a lowercase character.
 #   - We require node labels and relationship types to start with an uppercase character.
 #   - We disallow underscores in node labels, relationship types, property names, variables. (Hint: use camelCase)
+#   - There is no support for undirected edges
 
 COMMA, COLON, LPAR, RPAR, LBRACE, RBRACE, EQUAL = map(Suppress, ",:(){}=")
 
@@ -15,9 +16,9 @@ accesskey = Combine(freevar + Literal('.') + attribute)
 label = Word(alphas.upper(), alphanums)
 
 IDElement = (constant | accesskey | freevar | label)
-# as list of constant or accesskey expressions
+# As list of constant or accesskey expressions:
 #ConstExpression = DelimitedList(accesskey | constant, delim="+")
-# flatened Constexpression
+# Or, flatened Constexpression:
 ConstExpression = Combine((accesskey | constant) + ZeroOrMore("+" + (accesskey | constant)), adjacent=False, joinString=" ")
 PropertyElement = Group(attribute('key') + EQUAL + ConstExpression('value'))
 
@@ -25,8 +26,11 @@ IDTuple = LPAR + ZeroOrMore(IDElement + Optional(COMMA))('ids') + RPAR
 Labels = OneOrMore(label + Optional(COMMA))('labels')
 PropertyList = LBRACE + OneOrMore(PropertyElement + Optional(COMMA))('properties') + RBRACE
 
-ContentConstructor = LPAR + Optional(freevar('alias') + EQUAL) + IDTuple + COLON + Optional(Labels) + Optional(PropertyList) + RPAR 
-NodeConstructor = ContentConstructor | (LPAR + freevar('alias') + RPAR)
+ContentConstructor = Optional(freevar('alias') + EQUAL) + IDTuple + COLON + Optional(Labels) + Optional(PropertyList) 
+NodeConstructor = LPAR + (ContentConstructor | freevar('alias')) + RPAR
+RightEdgeConstructor = Group(NodeConstructor)('src') + Suppress('-[') + Group(ContentConstructor)('edge') + Suppress(']->') + Group(NodeConstructor)('tgt')
+LeftEdgeConstructor = Group(NodeConstructor)('tgt') + Suppress('<-[') + Group(ContentConstructor)('edge') + Suppress(']-') + Group(NodeConstructor)('src')
+EdgeConstructor = RightEdgeConstructor | LeftEdgeConstructor
 
 try:
     print("Test base elements:")
@@ -55,18 +59,24 @@ try:
     res = ConstExpression.parseString(' "test" + "cc" ')
     print(res.dump())
     print("Test ContentConstructor:")
-    res = ContentConstructor.parseString('(("test", x, x1.de1, x2.de2, x3.de3, ) : Person, State, { name = "test" + "cc", city = x.city, } ) ')
+    res = ContentConstructor.parseString('("test", x, x1.de1, x2.de2, x3.de3, ) : Person, State, { name = "test" + "cc", city = x.city, }  ')
     print(res.asDict())
-    res = ContentConstructor.parseString('(w = (x) : { name = x.name } ) ')
+    res = ContentConstructor.parseString('w = (x) : { name = x.name }  ')
     print(res.asDict())
-    res = ContentConstructor.parseString('(x = () : ) ')
+    res = ContentConstructor.parseString('x = () : ')
     print(res.asDict())
-    res = ContentConstructor.parseString('(() : )')
+    res = ContentConstructor.parseString('() : ')
     print(res.asDict())
     print("Test NodeConstructor:")
-    res = NodeConstructor.parseString('(("test", x, x1.de1, x2.de2, x3.de3, ) : Person, State, { name = "test" + x.name  , city = x.city + y.va, } ) ')
+    res = NodeConstructor.parseString('( w = ("test", x, x1.de1, x2.de2, x3.de3, ) : Person, State, { name = "test" + x.name  , city = x.city + y.va, } ) ')
     print(res.asDict())
     res = NodeConstructor.parseString('(x)')
+    print(res.asDict())
+    print("Test RightEdgeConstructor:")
+    res = RightEdgeConstructor.parseString('( (x) : Person { name = x.name }  ) -[ (x.a, "test") : HAS { name = x.name } ]-> (w)')
+    print(res.asDict())
+    print("Test EdgeConstructor:")
+    res = EdgeConstructor.parseString('((x):P{n=x.n})<-[(x.a,"t"):H,T{m=x.m+y.c+"3"}]-(w=("r",x,x1.de1,L):P,S,{n="u"+x.n,c=x.c+y.va,})')
     print(res.asDict())
 except ParseException as pe:
     print("Did not Match: ", pe)
