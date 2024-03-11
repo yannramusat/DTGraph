a0c66ba01569ff856a89aca5ac8a4e7143060a2b
yannramusat <yann.ramusat@ens.fr>
2024-03-11 19:09:16 +0100

    Tried managed transformations to avoid critical errors on detach delete but database stopped working.

--- dtgraph/backend/neo4j/graph.py
+++ dtgraph/backend/neo4j/graph.py
@@ -40,18 +40,22 @@ class Neo4jGraph(object):
         print(f"Flushed database: Deleted {summary.counters.nodes_deleted} nodes, deleted {summary.counters.relationships_deleted} relationships, completed after {summary.result_available_after} ms.")
 
     def abort(self, stats=False):
-        abort_query = """
-        MATCH (n:`_dummy`)
-        DETACH DELETE n
-        """
-        records, summary, keys = self.driver.execute_query(
-            abort_query,
-            database=self.database)
-        if(self.verbose):
-            self.print_query_stats(records, summary, keys)
-        if(stats):
-            print(f"Abort: Deleted {summary.counters.nodes_deleted} nodes, deleted {summary.counters.relationships_deleted} relationships, completed after {summary.result_available_after} ms.")
-    
+        with self.driver.session(database=self.database) as session:
+            abort_query = """
+            MATCH (n:`_dummy`)
+            CALL { WITH n
+            DETACH DELETE n
+            } IN TRANSACTIONS OF 10000 ROWS
+            """
+            #records, summary, keys = 
+            session.run(
+                abort_query)
+                #database=self.database)
+            #if(self.verbose):
+            #    self.print_query_stats(records, summary, keys)
+            #if(stats):
+            #    print(f"Abort: Deleted {summary.counters.nodes_deleted} nodes, deleted {summary.counters.relationships_deleted} relationships, completed after {summary.result_available_after} ms.")
+        
     def destruct_input(self, stats = False):
         destruct_query = """
         MATCH (n)
